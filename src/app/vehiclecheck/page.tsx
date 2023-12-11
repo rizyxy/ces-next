f83aa9a0e1fbdfe021e2bce4aaac0ac06c15bbfe
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-12-11 23:38:08 +0700

    adding form

--- src/app/vehiclecheck/page.tsx
+++ src/app/vehiclecheck/page.tsx
@@ -7,20 +7,49 @@ export default function Page() {
     <>
       <div className={styles.container}>
         <div className={styles.kontenBox}>
-          <h1 className={styles.title}>Vehicle Check</h1>
+          <h1 className={styles.title}>Masukkan Informasi Kendaraan</h1>
           <div className={styles.konten}>
             <form className={`${styles["form"]}`} action="">
-              <div className="">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  NIK
+              <div className={`mb-5`}>
+                <label htmlFor="exampleInputEmail1" className={`form-label`}>
+                  Provinsi
                 </label>
                 <input
                   type="text"
-                  className="form-control"
+                  className={`form-control`}
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                 />
               </div>
+              <div className={`mb-5`}>
+                <label htmlFor="exampleInputEmail1" className={`form-label`}>
+                  Kota
+                </label>
+                <input
+                  type="text"
+                  className={`form-control`}
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div className={`mb-5`}>
+                <label htmlFor="exampleInputEmail1" className={`form-label`}>
+                  Nomor Polisi
+                </label>
+                <input
+                  type="text"
+                  className={`form-control`}
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <center>
+                <Link href="/payment/bills">
+                  <button type="submit" className={styles.payButton}>
+                    Cek
+                  </button>
+                </Link>
+              </center>
             </form>
           </div>
         </div>
