ff0460317083081d0a50938a889ff89bf023bbdc
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-12-12 09:33:14 +0700

    bills revision

--- src/app/payment/bills/page.tsx
+++ src/app/payment/bills/page.tsx
@@ -1,52 +1,160 @@
-import Link from 'next/link'
-import styles from './bills.module.css'
+import Link from "next/link";
+import styles from "./bills.module.css";
 
 export default function Page() {
-    return <>
-        <section className={styles.vehicleData}>
-            <h2>Data Kendaraan</h2>
-            <div className={styles.vdataItem}>
-                <p  className={styles.vdataTitle}>Nama Pemilik</p>
-                <p  className={styles.vdataValue}>Mukhammad Rizki</p>
-            </div>
-            <div  className={styles.vdataItem}>
-                <p  className={styles.vdataTitle}>NIK Pemilik</p>
-                <p  className={styles.vdataValue}>082111633057</p>
-            </div>
-            <div  className={styles.vdataItem}>
-                <p  className={styles.vdataTitle}>Model Kendaraan</p>
-                <p  className={styles.vdataValue}>Aventador</p>
-            </div>
-            <div  className={styles.vdataItem}>
-                <p  className={styles.vdataTitle}>Merk Kendaraan</p>
-                <p  className={styles.vdataValue}>Lamborghini</p>
-            </div>
-            <div  className={styles.vdataItem}>
-                <p  className={styles.vdataTitle}>Tahun Kendaraan</p>
-                <p  className={styles.vdataValue}>2014</p>
-            </div>
-        </section>
+  return (
+    <>
+      <div className={`${styles["container"]}`}>
+        <div className={`${styles["konten-box"]}`}>
+          <h1 className={`${styles["title"]}`}>Pembayaran Pajak Kendaraan</h1>
+          <div className={`${styles["konten"]}`}>
+            <form className={`${styles["form"]}`} action="">
+              <div className={`${styles["form-box"]}`}>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Nama Pemilik
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Woody Andy"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    NIK Pemilik
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="082111633057"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Model Kendaraan
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Fortuner"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Merk Kendaraan
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Toyota"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Tahun
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="2020"
+                  />
+                </div>
+                <center className="my-4">
+                  <div className={`${styles["line"]}`}></div>
+                </center>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Pajak
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Rp6.500.000,00"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Biaya Admin
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]}`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Rp100.000,00"
+                  />
+                </div>
+                <div className={`${styles["form-group"]} mb-3`}>
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className={`${styles["form-labels-text"]}`}
+                  >
+                    Total
+                  </label>
+                  <input
+                    disabled
+                    type="text"
+                    className={`${styles["form-control"]} fw-bold text-black`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value="Rp6.600.000,00"
+                  />
+                </div>
+              </div>
 
-        <section className={styles.bills}>
-            <h2>Rincian Tagihan</h2>
-            <div className={styles.billItem}>
-                <p className={styles.billTitle}>Pajak</p>
-                <p className={styles.billValue}>Rp 100.000.000</p>
-            </div>
-            <div className={styles.billItem}>
-                <p className={styles.billTitle}>Biaya Admin</p>
-                <p className={styles.billValue}>Rp 100.000</p>
-            </div>
-            <hr></hr>
-            <div className={styles.billItem}>
-                <p className={styles.billTitle}>Total</p>
-                <p className={styles.billValue}>Rp 100.100.000</p>
-            </div>
-            <Link href='/payment/methods'>
-            <button className={styles.payButton}>
-                <p>Bayar</p>
-            </button>
-            </Link>
-        </section>
+              <div className={`${styles["button-group"]}`}>
+                <Link href="/vehiclecheck">
+                  <button className={styles.canButton}>Batal</button>
+                </Link>
+                <Link href="/payment/methods">
+                  <button className={styles.payButton}>Bayar</button>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
+  );
 }
