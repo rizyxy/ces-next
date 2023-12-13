3d7512b9d2bfc4cf182405f6627cf3708aeb4912
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-12-13 09:42:29 +0700

    fixing minor

--- src/app/payment/methods/page.tsx
+++ src/app/payment/methods/page.tsx
@@ -4,83 +4,84 @@ import styles from "./paymentmethod.module.css";
 export default function Page() {
   return (
     <>
-      <h2 className={styles.title}>Metode Pembayaran</h2>
-
-      <section className={styles.paymentMethods}>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 9.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 8.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 21.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 20.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 19.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 17.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 15.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-        <Link href="/payment/invoice">
-          <div className={styles.paymentMethod}>
-            <img
-              className={styles.paymentImg}
-              src="/assets/payment/image 13.png"
-              alt=""
-            ></img>
-          </div>
-        </Link>
-      </section>
+    <div className={styles.containers}>
+      
+        <h2 className={styles.title}>Metode Pembayaran</h2>
+        <section className={styles.paymentMethods}>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 9.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 8.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 21.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 20.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 19.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 17.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 15.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+          <Link href="/payment/invoice">
+            <div className={styles.paymentMethod}>
+              <img
+                className={styles.paymentImg}
+                src="/assets/payment/image 13.png"
+                alt=""
+              ></img>
+            </div>
+          </Link>
+        </section>
+    </div>
     </>
   );
 }
